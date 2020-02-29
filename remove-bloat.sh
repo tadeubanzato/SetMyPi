e310e6a8e952d4ab3416c8d4b41a746b43770ca4
tadeubanzato <tadeubanzato@gmail.com>
2020-02-28 22:55:33 -0500

    Update remove-bloat.sh

--- remove-bloat.sh
+++ remove-bloat.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# Remove bloatware (Wolfram Engine, Libre Office, Minecraft Pi, sonic-pi dillo gpicview penguinspuzzle oracle-java8-jdk openjdk-7-jre oracle-java7-jdk openjdk-8-jre)
-sudo apt-get remove --purge wolfram-engine libreoffice* scratch* minecraft-pi sonic-pi dillo gpicview oracle-java8-jdk openjdk-7-jre oracle-java7-jdk openjdk-8-jre -y
+# Remove bloatware (Wolfram Engine, Libre Office, Minecraft Pi, sonic-pi dillo gpicview penguinspuzzle)
+sudo apt-get remove --purge wolfram-engine libreoffice* scratch* minecraft-pi sonic-pi dillo gpicview penguinspuzzle -y
 
 # Autoremove
 sudo apt-get autoremove -y
@@ -11,3 +11,11 @@ sudo apt-get autoclean -y
 
 # Update
 sudo apt-get update
+
+# Install Samba
+sudo apt-get install samba samba-common-bin
+
+#MK Dir Share
+mkdir ~/Share
+
+
