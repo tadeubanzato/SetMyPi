e5297e49b2e0be20037f6ca2d4c8cfec80d31672
tadeubanzato <tadeubanzato@gmail.com>
2020-03-01 11:07:03 -0500

    Update kiosk.sh

--- kiosk.sh
+++ kiosk.sh
@@ -1,21 +1,5 @@
 sudo nano /home/pi/kiosk.sh
 
-#!/bin/bash
-xset s noblank
-xset s off
-xset -dpms
-
-unclutter -idle 0.5 -root &
-
-sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' /home/pi/.config/chromium/Default/Preferences
-sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' /home/pi/.config/chromium/Default/Preferences
-
-/usr/bin/chromium-browser --noerrdialogs --disable-infobars --kiosk https://datastudio.google.com/embed/reporting/d4fbfe6c-fdf1-41f7-a4f6-a972959261eb/page/a5o1 https://datastudio.google.com/embed/reporting/6627889d-3abc-4cb9-8e1a-ec3ba3fe8646/page/a5o1 https://datastudio.google.com/embed/reporting/84984260-6b53-429b-a5b5-7921996a8b57/page/a5o1 https://datastudio.google.com/embed/reporting/cd30b870-6f63-4dd2-9ed6-6eada083d4b9/page/a5o1 https://datastudio.google.com/embed/reporting/46b0b8e2-3e54-4239-bda8-9fdd3a4f6393/page/a5o1 &'
-
-while true; do
-   xdotool keydown ctrl+Tab; xdotool keyup ctrl+Tab;
-   sleep 10
-done
-#sudo cp /home/pi/kiosk.sh /home/pi/
+sudo echo "test
 
 printf "\n\n### CREATED kiosk.sh /home/pi/ ####\n\n"
