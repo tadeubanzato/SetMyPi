e3b7d35da58bc85eee1df8e52b025446aab65f51
tadeubanzato <tadeubanzato@gmail.com>
2020-03-10 18:39:51 -0400

    Update basicpack.sh

--- basicpack.sh
+++ basicpack.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
-#Run command will clean up the pi and add the Samba server configs
-#sudo curl -fsSL https://raw.githubusercontent.com/tadeubanzato/SetMyPi/master/basicpack.sh | bash
-#curl -OL https://raw.githubusercontent.com/tadeubanzato/DashPi/blob/master/kiosk.sh
-
-# Remove bloatware (Wolfram Engine, Libre Office, Minecraft Pi, sonic-pi dillo gpicview penguinspuzzle)
-sudo apt-get remove --purge libreoffice* minecraft-pi sonic-pi dillo gpicview penguinspuzzle -y
-
-# Autoremove  & Celan
-sudo apt-get autoremove -y
-sudo apt-get autoclean -y
-
-# Update and Upgrade Distribution
-sudo apt-get update && sudo apt-get dist-upgrade
-
+# #Run command will clean up the pi and add the Samba server configs
+# #sudo curl -fsSL https://raw.githubusercontent.com/tadeubanzato/SetMyPi/master/basicpack.sh | bash
+# #curl -OL https://raw.githubusercontent.com/tadeubanzato/DashPi/blob/master/kiosk.sh
+#
+# # Remove bloatware (Wolfram Engine, Libre Office, Minecraft Pi, sonic-pi dillo gpicview penguinspuzzle)
+# sudo apt-get remove --purge libreoffice* minecraft-pi sonic-pi dillo gpicview penguinspuzzle -y
+#
+# # Autoremove  & Celan
+# sudo apt-get autoremove -y
+# sudo apt-get autoclean -y
+#
+# # Update and Upgrade Distribution
+# sudo apt-get update && sudo apt-get dist-upgrade
 
 # Start Samba Instalation -------------
 sudo apt-get install samba samba-common-bin  -y
