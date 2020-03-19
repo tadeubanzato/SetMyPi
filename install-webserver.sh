6777ef4f7d240498804d7da8f7bd34470ce59b2d
tadeubanzato <tadeubanzato@gmail.com>
2020-03-19 10:45:58 -0400

    Update install-webserver.sh

--- install-webserver.sh
+++ install-webserver.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 ## Remove Apache
-sudo apt-get remove apache*
-sudo apt-get autoremove
+sudo apt-get remove apache* -y
+sudo apt-get autoremove -y
 
 ## Install NGINX from repository and start NGINX
-sudo apt-get install nginx
+sudo apt-get install nginx -y
 sudo systemctl start nginx
 
 ## No PHP support is installed, if you want to install PHP follow steps at https://pimylifeup.com/raspberry-pi-nginx/
