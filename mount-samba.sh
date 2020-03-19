0a430686c68c6135a8af9db778630a664eaf4c0b
tadeubanzato <tadeubanzato@gmail.com>
2020-03-19 11:28:44 -0400

    Update mount-samba.sh

--- mount-samba.sh
+++ mount-samba.sh
@@ -4,6 +4,8 @@
 sudo mkdir -p /home/pi/webserver
 
 # Add the bind to /etc/fstab
-sudo echo "/var/www/html /home/pi/webserver none bind 0 0" >> /etc/fstab
+sudo cp /etc/fstab /tmp/fstab
+sudo echo "/var/www/html /home/pi/webserver none bind 0 0" >> /tmp/fstab
+sudo cp /tmp/fstab /etc/fstab
 sudo mount -o bind /var/www/html /home/pi/webserver
 sudo chown pi:pi /home/pi/webserver
