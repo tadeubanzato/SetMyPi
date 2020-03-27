cb53460648f0fadec011b190119a6fd5189c9943
tadeubanzato <tadeubanzato@gmail.com>
2020-03-27 10:57:49 -0400

    Update install-python.sh

--- install-python.sh
+++ install-python.sh
@@ -5,7 +5,7 @@ sudo apt install python-pip python-dev python-rpi.gpio -y
 sudo apt-get install build-essential checkinstall tk-dev libncurses5-dev libreadline-gplv2-dev libncursesw5-dev libsqlite3-dev libreadline6-dev libdb5.3-dev libgdbm-dev libsqlite3-dev libssl-dev libbz2-dev libexpat1-dev liblzma-dev zlib1g-dev libffi-dev libc6-dev -y
 
 ## Download Python 3.8.2
-wget https://www.python.org/ftp/python/3.8.0/Python-3.8.2.tgz
+wget https://www.python.org/ftp/python/3.8.2/Python-3.8.2.tgz
 sudo tar zxf Python-3.8.2.tgz
 cd Python-3.8.2
 sudo ./configure --enable-optimizations
@@ -23,10 +23,6 @@ pip install logging
 
 ## Install Djangoe
 pip install django
-
-## Install uWSGI
-pip install uwsgi
-# sudo -u www-data uwsgi --ini uwsgi.ini --need-app
 
 ## Install oAuth for Google API
 pip install oauth2client
