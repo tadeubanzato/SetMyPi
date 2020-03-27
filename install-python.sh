c7df38a6bed9b950acbe15d0714bced8b1d21e78
tadeubanzato <tadeubanzato@gmail.com>
2020-03-27 10:09:12 -0400

    Update install-python.sh

--- install-python.sh
+++ install-python.sh
@@ -2,7 +2,6 @@
 
 ## Install Python 2 but updates to 3.8.2
 sudo apt install python3-picamera python-pip python-dev python-rpi.gpio -y
-sudo apt-get install sudo apt-get install build-essential checkinstall -y
 sudo apt-get install build-essential checkinstall tk-dev libncurses5-dev libreadline-gplv2-dev libncursesw5-dev libsqlite3-dev libreadline6-dev libdb5.3-dev libgdbm-dev libsqlite3-dev libssl-dev libbz2-dev libexpat1-dev liblzma-dev zlib1g-dev libffi-dev libc6-dev -y
 
 ## Install Tweepy
