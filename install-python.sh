3ba3b0fe4fba4afca88c9f0b035f2f0881e3964c
tadeubanzato <tadeubanzato@gmail.com>
2020-03-27 10:25:14 -0400

    Update install-python.sh

--- install-python.sh
+++ install-python.sh
@@ -4,7 +4,7 @@
 sudo apt install python3-picamera python-pip python-dev python-rpi.gpio -y
 sudo apt-get install build-essential checkinstall tk-dev libncurses5-dev libreadline-gplv2-dev libncursesw5-dev libsqlite3-dev libreadline6-dev libdb5.3-dev libgdbm-dev libsqlite3-dev libssl-dev libbz2-dev libexpat1-dev liblzma-dev zlib1g-dev libffi-dev libc6-dev -y
 
-# Download Python 3.8.2
+## Download Python 3.8.2
 wget https://www.python.org/ftp/python/3.8.0/Python-3.8.2.tgz
 sudo tar zxf Python-3.8.2.tgz
 cd Python-3.8.2
@@ -21,7 +21,7 @@ pip install tweepy
 ## Install Logging
 pip install logging
 
-## Install Django
+## Install Djangoe
 pip install django
 
 ## Install uWSGI
