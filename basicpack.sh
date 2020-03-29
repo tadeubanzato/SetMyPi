6d0dfe1286ffc7ede19eead3e4e8472109761e55
tadeubanzato <tadeubanzato@gmail.com>
2020-03-28 20:17:15 -0400

    python 3.7

--- basicpack.sh
+++ basicpack.sh
@@ -26,12 +26,6 @@ sudo curl -fsSL https://raw.githubusercontent.com/tadeubanzato/SetMyPi/master/in
 printf "\n\n ${GREEN}--------> PASSED SAMBA INSTALL${NC}\n\n"
 sleep 4
 
-## Install Python 3.8.2
-printf "\n\n ${YELLOW}--------> STARTING PYTHON INSTALL${NC}\n\n"
-sudo curl -fsSL https://raw.githubusercontent.com/tadeubanzato/SetMyPi/master/install-python.sh | bash
-printf "\n\n ${GREEN}--------> PASSED PYTHON INSTALL${NC}\n\n"
-sleep 4
-
 ## Install Git
 printf "\n\n ${YELLOW}--------> STARTING GIT INSTALL${NC}\n\n"
 sudo apt-get install git -y
@@ -49,6 +43,12 @@ printf "\n\n ${YELLOW}--------> START BINDING Samba Share${NC}\n\n"
 sudo curl -fsSL https://raw.githubusercontent.com/tadeubanzato/SetMyPi/master/mount-samba.sh | bash
 printf "\n\n ${GREEN}--------> BINDED /var/www/html to Samba share${NC}\n\n"
 sleep 4
+
+# ## Install Python 3.7
+# printf "\n\n ${YELLOW}--------> STARTING PYTHON INSTALL${NC}\n\n"
+# sudo curl -fsSL https://raw.githubusercontent.com/tadeubanzato/SetMyPi/master/install-python.sh | bash
+# printf "\n\n ${GREEN}--------> PASSED PYTHON INSTALL${NC}\n\n"
+# sleep 4
 
 sudo apt install curl openssh-server ca-certificates apt-transport-https postfix -y
 
--- install-python.sh
+++ install-python.sh
@@ -11,22 +11,10 @@ sudo apt install python-pip python-dev python-rpi.gpio -y
 sudo apt-get install build-essential checkinstall tk-dev libncurses5-dev libreadline-gplv2-dev libncursesw5-dev libsqlite3-dev libreadline6-dev libdb5.3-dev libgdbm-dev libsqlite3-dev libssl-dev libbz2-dev libexpat1-dev liblzma-dev zlib1g-dev libffi-dev libc6-dev -y
 
 ## Download Python 3.8.2
-printf "\n\n ${YELLOW}--------> DOWNLOADING PYTHON 3.8.2 ${NC}\n\n"
-wget https://www.python.org/ftp/python/3.8.2/Python-3.8.2.tgz
-sudo tar zxf Python-3.8.2.tgz
-cd /home/pi/Python-3.8.2/
-sudo ./configure --enable-optimizations
-printf "\n\n ${YELLOW}--------> CONFIGURING PYTHON 3.8.2 AS STANDARD ${NC}\n\n"
-sudo make -j 4
-sudo make altinstall
-echo "alias python=/usr/local/bin/python3.8" >> ~/.bashrc
-source ~/.bashrc
-
-sudo apt install idle3
+printf "\n\n ${YELLOW}--------> INSTALLING PYTHON 3.7 ${NC}\n\n"
+sudo apt install python3 idle3
 
 printf "\n\n ${YELLOW}--------> INSTALLING PYTHON MODULES ${NC}\n\n"
-
-## Install Tweepy
 pip install tweepy logging django oauth2client gspread requests pprint
 
 # ## Install Logging
